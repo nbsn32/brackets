1a84d7587e49a88a683b0ac7fb617cf40a067c33
Andrei Petrushin <andrei.petrushin@gmail.com>
2021-03-12 23:48:34 +0300

    feat: task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length % 2 != 0)
+      return false;
+
+  for (let i = 1; i < str.length; i++) {
+      let chCurrent = str[i],
+          chPrevious = str[i - 1];
+
+      for (let config of bracketsConfig) {
+          if (chPrevious === config[0] && chCurrent === config[1])
+              return check(str.slice(0, i - 1) + str.slice(i + 1), bracketsConfig);
+      }
+  }
+
+  return !str.length;
+};
